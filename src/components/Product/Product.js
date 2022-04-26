16c9653471c8e15066a8d13a2f48f4b71c527cf8
Katarzyna Sobieska <kat.sobieska@gmail.com>
2022-04-26 20:01:17 +0200

    Add useMemo for currentPrice

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,6 @@
 import styles from "./Product.module.scss";
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import ProductImage from "../ProductImage/ProductImage";
 import ProductForm from "../ProductForm/ProductForm";
 
@@ -8,6 +8,7 @@ const Product = (props) => {
   const [currentColor, setCurrentColor] = useState(props.colors[0]);
   const [currentSize, setCurrentSize] = useState(props.sizes[0].name);
   const [currentPrice, setCurrentPrice] = useState(props.basePrice);
+  const [additionalPrice, setAdditionalPrice] = useState("");
 
   Product.propTypes = {
     id: PropTypes.number,
@@ -19,8 +20,12 @@ const Product = (props) => {
   };
 
   const getPrice = (price) => {
-    return setCurrentPrice(props.basePrice + price);
+    return setAdditionalPrice(price);
   };
+
+  useMemo(() => {
+    return setCurrentPrice(props.basePrice + additionalPrice);
+  }, [props.basePrice, additionalPrice]);
 
   return (
     <article className={styles.product}>
